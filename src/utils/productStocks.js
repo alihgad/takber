5c17f172a39457ecd7606ec0995ce969583d6b17
alihgad <alihgad2@gmail.com>
2025-07-19 16:22:46 +0300

    fix stock calculation

--- src/utils/productStocks.js
+++ src/utils/productStocks.js
@@ -15,30 +15,10 @@ export const getProductStocks = async (products , filter = {}) => {
         let stock = await stockModel.find({ productId: product._id })
       
         if (stock.length != 0) {
-
-            for (const s of stock) {
-                
-                if (color && s.color != color) {
-                    continue
-                }
-                if (size && s.size != size) {
-                    continue
-                }
-
-                product.quantity = s.quantity
-                product.color = s.color
-                product.size = s.size
-
-                result.push(product)
-
-            }
-
-        } else {
-            product.quantity = 0
-            product.color = "N/A"
-            product.size = "N/A"
+            product.stock = stock
             result.push(product)
-
+        } else {
+            result.push(product)
         }
 
 

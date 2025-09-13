09113eae6da9c7e14c065868edadd301d1d3d87f
abdelfatah403 <abdelfatahmoharem@gmail.com>
2025-09-13 17:45:34 +0300

    fix delete image

--- src/services/deleteImage.js
+++ src/services/deleteImage.js
@@ -1,6 +1,5 @@
-const fs = require('fs');
-const path = require('path');
-
+import fs from 'fs';
+import path from 'path';
 /**
  * Delete an image file from the filesystem
  * @param {string} imagePath - The path to the image file to delete

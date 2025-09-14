ea00a33a04d06a1630128d03d1a33b37ad30adfc
alihgad <alihgad2@gmail.com>
2025-09-14 21:31:38 +0300

    .

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -11,7 +11,7 @@ export const multerHost = (path) => {
     const storage = multer.diskStorage({
         filename: (req, file, cb) => {
             let [name, ext] = file.originalname.split(".")
-            cb(null, name + "-" + Date.now() + ext)
+            cb(null, name + "-" + Date.now() + "." + ext)
         },
         destination: "uploads/" + path
     })

177008929f171dae9ca28721e3e922b4313aef17
alihgad <alihgad2@gmail.com>
2025-10-12 21:46:21 +0300

    null

--- src/moduels/order/order.controller.js
+++ src/moduels/order/order.controller.js
@@ -30,7 +30,7 @@ export let orderRouter = Router()
  * @apiSuccess {Object} order Created order details with populated product and cart information
  * @apiError {String} message Error message if cart is empty or stock insufficient
  */
-orderRouter.post("/", validation(createOrderSchema), asyncHandler(os.createOrder))
+orderRouter.post("/",(req,res,next)=>{ if(req.body.email.length === 0) req.body.email = undefined; next()} ,  validation(createOrderSchema), asyncHandler(os.createOrder))
 
 /**
  * @api {get} /order/user Get user's orders
